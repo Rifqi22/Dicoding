572c52ca12a39290b5830b3e6fcd2492dda12f30
Rifqi22 <rifqinaufalfayyadh@gmail.com>
2023-10-02 18:44:13 +0700

    commit try.js, adding function to subtract

--- Practice/JavaScript/try.js
+++ Practice/JavaScript/try.js
@@ -3,3 +3,16 @@ const sum = (a, b) =>{
 }
 const result = sum(4,4)
 console.log(result);
+
+const subtract = (a, ...b) => {
+    let result = 0;
+    if (a < b){
+        result = b - a;
+    } else {
+        result = a - b;
+    }
+    return result;
+}
+
+const trySubtract = subtract(4, 2);
+console.log(trySubtract);
