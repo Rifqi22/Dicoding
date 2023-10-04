eb88ffac38ca0a81ae50ba86a11dc1da725d0889
Rifqi22 <rifqinaufalfayyadh@gmail.com>
2023-10-04 12:07:49 +0700

    adding submission weather-finder

--- Practice/Front-End Web Development/ECMAScript/ClubFinder/src/script/data/data-source.js
+++ Practice/Front-End Web Development/ECMAScript/ClubFinder/src/script/data/data-source.js
@@ -1,18 +1,36 @@
 import { clubs } from "./clubs.js";
 
-class DataSource {
-    static searchClub(keyword) {
-        return new Promise((resolve, reject) => {
-            const filteredClubs = clubs.filter(function (club) {
-                return club.name.toUpperCase().includes(keyword.toUpperCase());
-            });
+// class DataSource {
+//     static searchClub(keyword) {
+//         return new Promise((resolve, reject) => {
+//             const filteredClubs = clubs.filter(function (club) {
+//                 return club.name.toUpperCase().includes(keyword.toUpperCase());
+//             });
 
-            if (filteredClubs.length) {
-                resolve(filteredClubs);
-            } else {
-                reject(`${keyword} is not found`);
-            }
-        })
+//             if (filteredClubs.length) {
+//                 resolve(filteredClubs);
+//             } else {
+//                 reject(`${keyword} is not found`);
+//             }
+//         })
+//     }
+// }
+
+// Using Fetch
+class DataSource {
+    static searchClub(keyword){
+        return fetch(`https://sports-api.dicoding.dev/teams/search?t=${keyword}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((responseJson)=>{
+                if(responseJson.teams){
+                    return Promise.resolve(responseJson.teams);
+                } else {
+                    return Promise.reject(`${keyword} is not found`);
+                }
+            })
+        ;
     }
 }
 
