ac9e82fcec2bb21ba8f0ed5f5e48d42a291d2082
Rifqi22 <rifqinaufalfayyadh@gmail.com>
2023-11-06 11:26:49 +0700

    commit front-end expert

---
+++ Practice/Front-End Web Expert/PWA/movie-catalogue/src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    KEY: '2cb755b3aa29c05cbad63069c4b67095',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+    CACHE_NAME: new Date().toISOString(),
+  };
+   
+  export default CONFIG;
